a0902070bc6b35ae5bffa3129284090cb0046739
yuhara327 <51362368+Yuhara327@users.noreply.github.com>
2021-08-12 23:04:40 +0900

    保持型プロパティ？

--- TestField.playground/Contents.swift
+++ TestField.playground/Contents.swift
@@ -1,31 +1,27 @@
 class Monster {
-    var name: String!
-    var hp: Int!
-    var df: Int!
+    var name: String
+    var hp: Int
+    var df: Int
+    init(name: String,hp: Int,df: Int){
+        self.name = name
+        self.hp = hp
+        self.df = df
+    }
     func display() {
-        print("\(name!)が現れた")
-        print("HP:\(hp!)")
-        print("DF:\(df!)")
+        print("\(name)が現れた")
+        print("HP:\(hp)")
+        print("DF:\(df)")
     }
 }
-let Monster1 = Monster()
-Monster1.name = "slime"
-Monster1.hp = 50
-Monster1.df = 100
+let Monster1 = Monster(name: "slime",hp: 50,df: 100)
 Monster1.display()
 
-let Monster2 = Monster()
-Monster2.name = "killerMachine"
-Monster2.hp = 200
-Monster2.df = 100
-Monster2.display()
 
-func Evolution (monsterNumber: Monster){
-    monsterNumber.hp += 100
-    monsterNumber.df += 50
-    print("\(monsterNumber.name!)が進化した")
-    print("HP:\(monsterNumber.hp!)")
-    print("DF:\(monsterNumber.df!)")
+func evolution (monster: Monster){
+    monster.hp += 100
+    monster.df += 50
+    print("\(monster.name)が進化した")
+    print("HP:\(monster.hp)")
+    print("DF:\(monster.df)")
 }
-Evolution(monsterNumber: Monster1)
-Evolution(monsterNumber: Monster2)
+evolution(monster: Monster1)
